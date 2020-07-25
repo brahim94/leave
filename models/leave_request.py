5fc1080c3baa41f8b16c6dd2b0822474f7eae8ef
brahim ayad <brahim-ayad@hotmail.com>
2020-07-25 03:42:55 -0700

    finalization

--- models/leave_request.py
+++ models/leave_request.py
@@ -10,6 +10,7 @@ class Leave_request(models.Model):
 
     
     phone_number = fields.Char(string='N° Téléphone')
+    #phone_number_work = fields.Char(string='N° Téléphone')
     ville = fields.Char(string='Ville')
     matricule_employee = fields.Many2one('hr.job', string="Matricule")
     interim_employee = fields.Many2one('hr.employee', string="Employé en intérim")
@@ -17,6 +18,7 @@ class Leave_request(models.Model):
     @api.onchange('employee_id')
     def onchange_employee_id(self):
         self.phone_number = self.employee_id.mobile_phone
+        #self.phone_number_work = self.employee_id.work_phone
         self.ville = self.employee_id.work_location
         self.matricule_employee = self.employee_id.job_id
         
